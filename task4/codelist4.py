016cf979bb9b896e08c5335906ac836c56937ea0
ivan <ivangoglev@protonmail.com>
2020-02-25 17:24:51 +0300

    'зцв'

---
+++ task4/codelist4.py
@@ -0,0 +1,127 @@
+import psycopg2
+import json
+import xml.etree.ElementTree as xml
+import task4.secret
+import task4.queries
+
+
+#Класс первоначальной обработки входных данных
+class InitialProcessing:
+    def jsonfile_to_dict(self,path: str) -> dict:
+            """
+
+            :param path: json file path
+            :return: dictionary of value
+
+            Данная функция парсит json-файл и перегоняет значения в словарь.
+            """
+            try:
+                with open(path) as file:
+                    value = json.load(file)
+            except Exception as e:
+                print(e)
+                print("Ошибка чтения файла")
+                exit()
+
+            return value
+class DatabaseInitialization:
+
+    def sql_execution(self,sql:str,tableName:str) -> bool:
+        try:
+            con = psycopg2.connect(database="postgres",user="postgres",password=task4.secret.password,host="127.0.0.1",port="5432")
+            cur = con.cursor()
+            cur.execute(sql)
+            print("Table"+tableName+" created successfully")
+            con.commit()
+            con.close()
+            return True
+        except Exception as e:
+            print (e)
+            return False
+        finally:
+            print('this is a work of '+tableName+' table')
+
+
+class DatabaseFilling:
+    def insert_value_to_rooms (roomdict:dict)->bool:
+        try:
+            con = psycopg2.connect(database="postgres", user="postgres", password=task4.secret.password,
+                                   host="127.0.0.1", port="5432")
+            cur = con.cursor()
+            for item in roomdict:
+                cur.execute("INSERT INTO ROOMS VALUES("+str(item['id'])+","+"'"+item['name']+"'"+")")
+            con.commit()
+            print("Value Inserting")
+
+            con.commit()
+            con.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def insert_value_to_students(stud) -> bool:
+        con = psycopg2.connect(database="postgres", user="postgres", password=task4.secret.password, host="127.0.0.1",port="5432")
+        cur = con.cursor()
+        for item in stud:
+           cur.execute("INSERT INTO STUDENTS VALUES('{birthday}','{id}','{name}','{room}','{sex}')".format(birthday=item["birthday"][0:10],id=item["id"],name=item["name"],room=item["room"],sex=item["sex"]))
+        con.commit()
+        print("Value Inserting")
+        con.commit()
+        return True
+    def test(SQL):
+        con = psycopg2.connect(database="postgres", user="postgres", password=task4.secret.password, host="127.0.0.1",
+                               port="5432")
+        cur = con.cursor()
+        cur.execute(SQL)
+        rows = cur.fetchall()
+        print(rows)
+        return True
+
+        con.commit()
+        con.close()
+
+class endWork:
+    def dropTable(tablename:str)->bool:
+        try:
+            con = psycopg2.connect(database="postgres", user="postgres", password=task4.secret.password,
+                                   host="127.0.0.1", port="5432")
+            cur = con.cursor()
+            cur.execute('drop table {tabl}'.format(tabl=tablename))
+            con.commit()
+            con.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+
+
+
+class Main:
+    def __init__(self):
+        try:
+            endWork.dropTable('rooms')
+            endWork.dropTable('students')
+        except:
+            pass
+        try:
+            initialProcessingObject = InitialProcessing()
+            databaseInitializationObject = DatabaseInitialization()
+            rooms = initialProcessingObject.jsonfile_to_dict('D:\\LeverX_Python_course\\task4\\src\\rooms.json')
+            students = initialProcessingObject.jsonfile_to_dict('D:\\LeverX_Python_course\\task4\\src\\students.json')
+            databaseInitializationObject.sql_execution(task4.queries.CREATE_ROOMS,'ROOMS')
+            databaseInitializationObject.sql_execution(task4.queries.CREATE_STUDENTS, 'STUDENTS')
+            DBFillingObject = DatabaseFilling
+            DBFillingObject.insert_value_to_rooms(rooms)
+            DBFillingObject.insert_value_to_students(students)
+            DatabaseFilling.test(task4.queries.SQL1)
+            DatabaseFilling.test(task4.queries.SQL2)
+            DatabaseFilling.test(task4.queries.SQL3)
+            DatabaseFilling.test(task4.queries.SQL4)
+        finally:
+            endWork.dropTable('rooms')
+            endWork.dropTable('students')
+
+Main()
+
---
+++ task4/queries.py
@@ -0,0 +1,39 @@
+SQL1='''
+SELECT r.name,s.name 
+    from students s, rooms r
+    where s.room = r.id
+    order by r.name
+'''
+
+SQL2='''
+SELECT t.room, avg((current_date - birthday)/365) age
+	FROM students t
+group by t.room
+order by age
+limit 5
+'''
+SQL3='''
+SELECT t.room, max(abs(s.birthday - t.birthday)/365) age
+	FROM students t, students s
+	where s.room = t.room and s.id < t.id
+	group by t.room
+	order by age desc
+        limit 5
+
+
+'''
+SQL4='''
+
+select t.* from rooms t
+	where exists(
+		select 1 from students s 
+			where s.room = t.id and s.sex = 'M'
+	) 
+	and 
+	exists(
+		select 1 from students s 
+			where s.room = t.id and s.sex = 'F'
+	) 
+'''
+CREATE_ROOMS='''CREATE TABLE ROOMS(ID INT PRIMARY KEY,NAME CHAR(10));'''
+CREATE_STUDENTS='''CREATE TABLE STUDENTS(BIRTHDAY DATE,ID INT PRIMARY KEY,NAME CHAR(29),ROOM INT,SEX CHAR(1));'''
